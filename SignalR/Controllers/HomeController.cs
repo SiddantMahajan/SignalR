deb34b9f71f287368f9422abc04107bd9f594c37
Siddant Mahajan <siddantm.ce18@stvincentngp.edu.in>
2024-02-27 17:18:15 +0530

    Add project files.

---
+++ SignalR/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿// Controllers/HomeController.cs
+
+using Microsoft.AspNet.SignalR;
+using SignalR.Hubs;
+using System.Web.Mvc;
+
+public class HomeController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public JsonResult SendMessage(string targetUserId, string message)
+    {
+        // Handle signaling (exchange SDP offers and answers) here
+        // You might store signaling data in memory or a database
+        // For simplicity, we just echo the message back
+        return Json(new { targetUserId, message });
+    }
+}
---
+++ SignalR/Hubs/SignalingHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalR.Hubs
+{
+    public class SignalingHub : Hub
+    {
+        public void SendMessage(string targetUserId, string message)
+        {
+            Clients.User(targetUserId).messageReceived(Context.ConnectionId, message);
+        }
+
+        public void StartCall(string targetUserId)
+        {
+            Clients.User(targetUserId).startCallReceived(Context.ConnectionId);
+        }
+    }
+}
